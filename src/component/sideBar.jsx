bca3df6042391c38d0318f64b0c024a4e181ae26
Fira0985 <firafisberhanu4@gmail.com>
2025-02-08 23:51:47 +0300

    setting up the basic UI

---
+++ src/component/sideBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/sideBar.css";
+
+
+function SideBar(){
+    return(
+        <>
+        <div className="sidebar">
+            <a href="">Home</a>
+            <a href="">Education</a>
+            <a href="">My Projects</a>
+            <a href="">About</a>
+            <a href="">Contact Me</a>
+            <a href="">Ask AI</a>
+        </div>
+        </>
+    )
+}
+
+export default SideBar;
